cff42d7a15c7f5241adb22ff89d5be38f6a4d67b
Fritz Lin <uxfritz@163.com>
2016-01-03 23:54:23 +0800

    dev-server + react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,41 @@ const webpack = require('webpack')
 const WebpackNotifierPlugin = require('webpack-notifier')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
+const PORT = 3119
+
 module.exports = {
   module: {
     preLoaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'eslint' },
     ],
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'react-hot!babel' },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.scss$/, loader: 'style!css!sass' },
     ]
   },
   entry: {
-    'entry.js': './src/entry.js'
+    'entry.js': [
+      'webpack-dev-server/client?http://localhost:'+ PORT,
+      'webpack/hot/only-dev-server',
+      './src/entry.js'
+    ]
   },
   output: {
     pathinfo: true,
     path: 'dist/',
+    publicPath: 'http://localhost:'+ PORT +'/',
     filename: '[name]',
     sourceMapFilename: '[name].map',
     libraryTarget: 'var'
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    colors: true,
+    progress: true,
+    port: PORT
   },
   devtool: 'cheap-module-source-map',
   target: 'node-webkit',
@@ -31,10 +46,10 @@ module.exports = {
     __dirname: true
   },
   resolve: {
-    alias: {
-      'react': 'react-lite',
-      'react-dom': 'react-lite',
-    },
+    // alias: {
+    //   'react': 'react-lite',
+    //   'react-dom': 'react-lite',
+    // },
     extensions: ['', '.js', '.jsx'],
     packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
   },
@@ -42,6 +57,7 @@ module.exports = {
     new WebpackNotifierPlugin(),
     new CleanWebpackPlugin(['dist/']),
     // new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.ExternalsPlugin('commonjs', [
       'babel-polyfill',
       'lodash',
