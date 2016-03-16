24fb78ee3a219031dec48e7ca0f145302bbbf313
Fritz Lin <uxfritz@163.com>
2016-03-17 00:03:39 +0800

    webpack默认external

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 'use strict'
 const path = require('path')
-const _ = require('lodash')
 const webpack = require('webpack')
 const WebpackNotifierPlugin = require('webpack-notifier')
-const deps = _.keys(require('./package.json').dependencies)
 
 const inlinedDeps = [
   'normalize.css',
@@ -12,10 +10,9 @@ const inlinedDeps = [
   'react-router',
   "react-redux",
 ]
-// const externalDeps = _.without(deps, ...inlinedDeps)
-const externalDeps = _.without.apply(null, [deps].concat(inlinedDeps))
 
 module.exports = {
+
   module: {
     preLoaders: [
       { test: /\.jsx?$/, include: path.resolve('src'), loader: 'eslint' },
@@ -26,11 +23,13 @@ module.exports = {
       { test: /\.scss$/, loader: 'style!css!sass' },
     ]
   },
+
   target: 'node-webkit',
   node: {
     __filename: true,
     __dirname: true
   },
+
   resolve: {
     alias: {
       // 不用react-lite 影响Editor:focus/blur
@@ -40,10 +39,22 @@ module.exports = {
     extensions: ['', '.js', '.jsx'],
     packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
   },
+
+  // 由于是nw/node环境 默认将依赖external
+  // https://github.com/fritx/os-web/blob/dev/task/webpack.server.js
+  externals: [
+    (ctx, req,  cb) => {
+      // if (resolve(ctx, req).indexOf(serverDir) !== 0) return cb()
+      if (/^\.\.?\//.test(req)) return cb()
+      if (inlinedDeps.indexOf(req) > -1) return cb()
+      cb(null, `commonjs ${req}`)
+    },
+  ],
+
   plugins: [
     new WebpackNotifierPlugin({ alwaysNotify: true }),
     // new webpack.NoErrorsPlugin(),
-    new webpack.ExternalsPlugin('commonjs', externalDeps),
+    // new webpack.ExternalsPlugin('commonjs', externalDeps),
     new webpack.ProvidePlugin({
       Promise: 'bluebird',
     }),
@@ -51,4 +62,4 @@ module.exports = {
     //   'rootDir': JSON.stringify(__dirname)
     // })
   ],
-};
+}
