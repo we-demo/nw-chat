7d7d06344fae125acc372d16c2904e85b48ddff8
Fritz <uxfritz@163.com>
2016-03-17 12:13:48 +0800

    emojiList提取

--- src/components/EditorForm.js
+++ src/components/EditorForm.js
@@ -7,7 +7,7 @@ import { saveEditor } from '../actions'
 // import { win } from '../desktop'
 import Editor from './Editor'
 import Panel from './Panel'
-import Emoji from './Emoji'
+import Emoji, { emojiList, emojiReplace } from './Emoji'
 
 
 export default class EditorForm extends Component {
@@ -126,12 +126,12 @@ export default class EditorForm extends Component {
               }}>
                 {
                   // [ '😂', '🍎', '😊', '💩' ]
-                  [ 'smile','blush','smiley','relaxed','smirk','heart_eyes','kissing_heart','kissing_closed_eyes','flushed','relieved','disappointed_relieved','grin','wink','stuck_out_tongue_winking_eye','stuck_out_tongue_closed_eyes','grinning','kissing','kissing_smiling_eyes','stuck_out_tongue','sleeping','worried','frowning','anguished','open_mouth','grimacing','confused','hushed','expressionless','unamused','sweat_smile','sweat','weary','pensive','disappointed','confounded','fearful','cold_sweat','persevere','cry','sob','joy','astonished','scream','tired_face','angry','rage','triumph','sleepy','yum','mask','sunglasses','dizzy_face','imp','smiling_imp','neutral_face','no_mouth','innocent','alien','yellow_heart','blue_heart','purple_heart','heart','green_heart','broken_heart','heartbeat','heartpulse','two_hearts','revolving_hearts','cupid','sparkling_heart','sparkles','star','star2','dizzy','boom','anger','exclamation','question','grey_exclamation','grey_question','zzz','dash','sweat_drops','notes','musical_note','fire','hankey','+1','-1','ok_hand','facepunch','fist','v','wave','open_hands','point_up','point_down','point_left','point_right','raised_hands','pray','point_up_2','clap','muscle' ]
-                  .map((kw) => {
+                  emojiList.map((kw) => {
                     const c = emojilib.lib[kw].char
                     return (
                       <li key={c} onClick={()=>{
-                        this.refs.editor.insertHTML(c)
+                        const html = emojiReplace(c)
+                        this.refs.editor.insertHTML(html)
                         this.refs.emojiPanel.close()
                       }}><Emoji char={c} /></li>
                     )
